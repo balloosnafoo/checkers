acf793b6a7b8e3622b44ad674c8d76c9a3d22d2f
Matthew Skinner <skinnerm@email.arizona.edu>
2015-07-02 15:20:42 -0700

    refactor sliding and jumping methods

--- piece.rb
+++ piece.rb
@@ -18,47 +18,18 @@ class Piece
   JUMPING_VECTORS = [[2, 2], [2, -2]]
 
   def jumping_moves
-    x, y = pos
-    # debugger
     JUMPING_VECTORS.each_with_object([]) do |vector, arr|
-      dx, dy = vector.map{ |v| v * direction }
-      cap_x, cap_y = vector.map{ |v| v * direction  / 2 }
-      to_pos = [x + dx, y + dy]
-      cap_pos = [x + cap_x, y + cap_y]
-      if board.on_board?(to_pos) && board.empty_square?(to_pos)
-        if !board.is_color?(cap_pos, color) && !board.empty_square?(cap_pos)
-          arr << to_pos
-        end
-      end
-
-      if king?
-        dx, dy = vector.map{ |v| v * -direction }
-        cap_x, cap_y = vector.map{ |v| v * -direction  / 2 }
-        to_pos = [x + dx, y + dy]
-        cap_pos = [x + cap_x, y + cap_y]
-        if board.on_board?(to_pos) && board.empty_square?(to_pos)
-          if !board.is_color?(cap_pos, color) && !board.empty_square?(cap_pos)
-            arr << to_pos
-          end
-        end
-      end
+      jump_for_vector(vector,  direction, arr, pos)
+      jump_for_vector(vector, -direction, arr, pos) if kinged
     end
   end
 
   SLIDING_VECTORS = [[1, 1], [1, -1]]
 
   def sliding_moves
-    x, y = pos
     SLIDING_VECTORS.each_with_object([]) do |vector, arr|
-      dx, dy = vector.map{ |v| v * direction }
-      to_pos = [x + dx, y + dy]
-      arr << to_pos if board.on_board?(to_pos) && board.empty_square?(to_pos)
-
-      if king?
-        dx, dy = vector.map{ |v| v * -direction }
-        to_pos = [x + dx, y + dy]
-        arr << to_pos if board.on_board?(to_pos) && board.empty_square?(to_pos)
-      end
+      slide_for_vector(vector,  direction, arr)
+      slide_for_vector(vector, -direction, arr) if kinged
     end
   end
 
@@ -88,5 +59,25 @@ class Piece
 
   private
   attr_reader :kinged, :pos, :direction, :board
+
+  def jump_for_vector(vector, dir_switch, arr, pos)
+    x, y = pos
+    dx, dy = vector.map{ |v| v * dir_switch }
+    cap_x, cap_y = vector.map{ |v| v * direction  / 2 }
+    to_pos = [x + dx, y + dy]
+    cap_pos = [x + cap_x, y + cap_y]
+    if board.on_board?(to_pos) && board.empty_square?(to_pos)
+      if !board.is_color?(cap_pos, color) && !board.empty_square?(cap_pos)
+        arr << to_pos
+      end
+    end
+  end
+
+  def slide_for_vector(vector, dir_switch, arr)
+    x, y = pos
+    dx, dy = vector.map{ |v| v * dir_switch }
+    to_pos = [x + dx, y + dy]
+    arr << to_pos if board.on_board?(to_pos) && board.empty_square?(to_pos)
+  end
 
 end
